456f0c2b47776a624f1a0296478993a91b3068a7
Karungappan RM <sivakarun17@icloud.com>
2025-03-17 01:15:43 +0530

    Delete Analyze Data in a Model Car Database with MySQL Workbench directory

--- Analyze Data in a Model Car Database with MySQL Workbench/SQL Scripts/7. Payment Trends.sql
+++
@@ -1,9 +0,0 @@
-/* Payment Trends */
-
-SELECT 
-    YEAR(p.paymentDate) AS year, 
-    MONTH(p.paymentDate) AS month, 
-    ROUND(SUM(p.amount) ) AS totalPayments
-FROM payments p
-GROUP BY year, month
-ORDER BY year DESC, month DESC;
